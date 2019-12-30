3b0aeac2a10b67cce7c82eb31dea8b1af3dd0284
javier.bolanos.ironbit <javier.bolanos@ironbit.com.mx>
2019-12-29 21:04:14 -0600

    Initial Commit

---
+++ KEntertainmentService/KEntertainmentService.h
@@ -0,0 +1,19 @@
+//
+//  KEntertainmentService.h
+//  KEntertainmentService
+//
+//  Created by Javier Bolaños on 12/29/19.
+//  Copyright © 2019 Javier Bolaños. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KEntertainmentService.
+FOUNDATION_EXPORT double KEntertainmentServiceVersionNumber;
+
+//! Project version string for KEntertainmentService.
+FOUNDATION_EXPORT const unsigned char KEntertainmentServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KEntertainmentService/PublicHeader.h>
+
+
